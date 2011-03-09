afd676ba692584dfdb528e3e9c06c820fc7ce54d
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-03-09 11:31:35 +0100

    added new example code and updated uml diagrams

--- code/TStyle.h
+++ code/TStyle.h
@@ -1,38 +1,45 @@
-class TStyle : public TNamed, public TAttLine, public TAttFill, public TAttMarker, public TAttText {
+class TStyle : public TNamed, 
+  public TAttLine, 
+  public TAttFill, 
+  public TAttMarker, 
+  public TAttText {
   //...
-   virtual Int_t    DistancetoPrimitive(Int_t px, Int_t py);
-   Int_t            GetNdivisions(Option_t *axis="X") const;
-   TAttText        *GetAttDate() {return &fAttDate;}
-   Color_t          GetAxisColor(Option_t *axis="X") const;
-   Color_t          GetLabelColor(Option_t *axis="X") const;
-   Style_t          GetLabelFont(Option_t *axis="X") const;
-   Float_t          GetLabelOffset(Option_t *axis="X") const;
-   Float_t          GetLabelSize(Option_t *axis="X") const;
-   Color_t          GetTitleColor(Option_t *axis="X") const;  //return axis title color of pad title color
-   Style_t          GetTitleFont(Option_t *axis="X") const;   //return axis title font of pad title font
-   Float_t          GetTitleOffset(Option_t *axis="X") const; //return axis title offset
-   Float_t          GetTitleSize(Option_t *axis="X") const;   //return axis title size
-   Float_t          GetTickLength(Option_t *axis="X") const;
+   virtual Int_t    DistancetoPrimitive(Int_t px, 
+                                        Int_t py);
+   Int_t            GetNdivisions(Option_t *) const;
+   TAttText        *GetAttDate() ;
+   Color_t          GetAxisColor(Option_t *) const;
+   Color_t          GetLabelColor(Option_t *) const;
+   Style_t          GetLabelFont(Option_t *) const;
+   Float_t          GetLabelOffset(Option_t *) const;
+   Float_t          GetLabelSize(Option_t *) const;
+   Color_t          GetTitleColor(Option_t *) const; 
+   Style_t          GetTitleFont(Option_t *) const;  
+   Float_t          GetTitleOffset(Option_t *) const;
+   Float_t          GetTitleSize(Option_t *) const;  
+   Float_t          GetTickLength(Option_t *) const;
 
-   Float_t          GetBarOffset() const {return fBarOffset;}
-   Float_t          GetBarWidth() const {return fBarWidth;}
-   Int_t            GetDrawBorder() const {return fDrawBorder;}
-   Float_t          GetEndErrorSize() const {return fEndErrorSize;}
-   Float_t          GetErrorX() const {return fErrorX;}
-   Bool_t           GetCanvasPreferGL() const {return fCanvasPreferGL;}
-   Color_t          GetCanvasColor() const {return fCanvasColor;}
-   Width_t          GetCanvasBorderSize() const {return fCanvasBorderSize;}
-   Int_t            GetCanvasBorderMode() const {return fCanvasBorderMode;}
-   Int_t            GetCanvasDefH() const      {return fCanvasDefH;}
-   Int_t            GetCanvasDefW() const      {return fCanvasDefW;}
-   Int_t            GetCanvasDefX() const      {return fCanvasDefX;}
-   Int_t            GetCanvasDefY() const      {return fCanvasDefY;}
+   Float_t          GetBarOffset() const ;
+   Float_t          GetBarWidth() const ;
+   Int_t            GetDrawBorder() const ;
+   Float_t          GetEndErrorSize() const ;
+   Float_t          GetErrorX() const ;
+   Bool_t           GetCanvasPreferGL() const ;
+   Color_t          GetCanvasColor() const ;
+   Width_t          GetCanvasBorderSize() const ;
+   Int_t            GetCanvasBorderMode() const ;
+   Int_t            GetCanvasDefH() const      ;
+   Int_t            GetCanvasDefW() const      ;
+   Int_t            GetCanvasDefX() const      ;
+   Int_t            GetCanvasDefY() const      ;
    Int_t            GetColorPalette(Int_t i) const;
-   Int_t            GetColorModelPS() const    {return fColorModelPS;}
-   Float_t          GetDateX()  const          {return fDateX;}
-   Float_t          GetDateY() const           {return fDateY;}
-   // ... 200 more functions ...
-   void             SaveSource(const char *filename, Option_t *option=0);
+   Int_t            GetColorModelPS() const    ;
+   Float_t          GetDateX()  const          ;
+   Float_t          GetDateY() const           ;
+   // ... 200 more functions including 
+   //     the setters for the above getters ...
+   void             SaveSource(const char *filename, 
+                               Option_t *option=0);
    //...
 };
 
