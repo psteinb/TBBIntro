d74094cb085abdc41e747164a631f6a5bda888c6
Peter Steinbach <Peter.Steinbach@tu-dresden.de>
2012-06-19 15:42:23 +0200

    finished simple

--- code/simple/simple.cc
+++ code/simple/simple.cc
@@ -36,6 +36,7 @@ int main(int argc, char* argv[])
    optDescription.add_options()
     ("help,h", "produce help message")
     ("nthreads,n",po::value<short>()->default_value(nMaxThreads),"number of threads to use")
+     ("iterations,i",po::value<unsigned int>()->default_value(0),"number of iterations")
     ;
 
   po::variables_map varMap;
@@ -44,17 +45,28 @@ int main(int argc, char* argv[])
  
   //////////////////////////////////////////////////////////////////////////////
   //PRINT USER INFO
-
-  std::cout << "running executable of " << __FILE__ << std::endl;
   short nThreads = varMap["nthreads"].as<short>();
   if(nThreads<1 || nThreads > nMaxThreads)
     nThreads = nMaxThreads;
 
-  std::cout << "with " << nThreads  << " threads" << std::endl;
-  
-  //////////////////////////////////////////////////////////////////////////////
-  //DO PARALLEL FOR
-  tbb::parallel_for(tbb::blocked_range<size_t>(0,nThreads,1),NumPrinter(nThreads));
+  unsigned int nIterations = varMap["iterations"].as<unsigned int>();
+  if(nIterations<1)
+    nIterations = nThreads;
+
+  std::cout << "running ";
+  if(nThreads==1){
+    std::cout << "serial version " << std::endl;
+    for(int i = 0;i<nIterations;i++)
+      std::cout << "iteration "<< i << std::endl;
+  }
+  else{
+    //////////////////////////////////////////////////////////////////////////////
+    //DO PARALLEL FOR
+    int grainsize = nIterations/nThreads;
+    std::cout << "parallel version with " << nThreads  << " threads, grainsize "<< grainsize << std::endl;
+    tbb::parallel_for(tbb::blocked_range<size_t>(0,nIterations,grainsize),NumPrinter());
+
+  }
   
   std::cout << "Done." << std::endl;
 
